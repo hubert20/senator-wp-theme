7f6629fdf838ae1e6bc3147dd7c6c9d9f8db3a3f
hubert20 <hubert@tomedia.pl>
2025-01-23 15:46:30 +0100

    Update main-counters.php

--- template-parts/main-counters.php
+++ template-parts/main-counters.php
@@ -1,6 +1,7 @@
 <?php
 $form_bg = get_field('form_bg');
 $form_cnt = get_field('form_cnt');
+$count_cnt = get_field('licznik_cnt');
 ?>
 
 <!-- Contact Form -->
@@ -10,30 +11,34 @@ $form_cnt = get_field('form_cnt');
     </div>
     <!-- Counters -->
     <div class="counter-main-box py-4 py-lg-5 bg-white" id="counter">
-        <h2 class="subtitle chakra-petch-font text-center standard-title-4 fw-bolder mb-3 mb-lg-5 position-relative" style="z-index: 0;">
-            Czemu zaufać właśnie nam?
+        <h2 class="subtitle chakra-petch-font text-center standard-title-4 fw-bolder mb-3 mb-lg-5 position-relative text-yellow" style="z-index: 0;">
+            Skorzystaj z naszego formularza kontaktowego!
         </h2>
+        <p class="subtitle chakra-petch-font text-center standard-title-4 fw-bolder mb-3 mb-lg-5 position-relative" style="z-index: 0;">
+            Jeśli masz pytania dotyczące naszej oferty lub chcesz złożyć zamówienie online, wypełnij formularz zapytania ofertowego. 
+            Skontaktujemy się z Tobą w celu realizacji zamówienia.
+        </p>
         <div class="row justify-content-center">
             <div class="col-lg-3">
                 <div class="position-relative counter-number--box">
                     <img src="/wp-content/uploads/2024/09/naprawa-alternatorow.png" class="img-fluid counter-number--icon" alt="">
-                    <p class="count counter-number counter-number--plus standard-title-3 chakra-petch-font fw-bold" data-count="6000">0</p>
+                    <p class="count counter-number counter-number--plus standard-title-3 chakra-petch-font fw-bold" data-count="12000">0</p>
                 </div>
-                <p class="text-uppercase fw-bold counter-main-box--titles lh-13">Naprawionych alternatorów</p>
+                <p class="text-uppercase fw-bold counter-main-box--titles lh-13">Wyprodukowanych szaf przesuwnych</p>
             </div>
             <div class="col-lg-3">
                 <div class="position-relative counter-number--box">
                     <img src="/wp-content/uploads/2024/09/naprawa-rozrusznikow.png" class="img-fluid counter-number--icon" alt="">
-                    <p class="count counter-number counter-number--plus standard-title-3 chakra-petch-font fw-bold" data-count="7000">0</p>
+                    <p class="count counter-number counter-number--plus standard-title-3 chakra-petch-font fw-bold" data-count="20">0</p>
                 </div>
-                <p class="text-uppercase fw-bold counter-main-box--titles lh-13">Naprawionych rozruszników</p>
+                <p class="text-uppercase fw-bold counter-main-box--titles lh-13">Lat w branży</p>
             </div>
             <div class="col-lg-3">
                 <div class="position-relative counter-number--box">
                     <img src="/wp-content/uploads/2024/09/klimatyzacja-naprawa-i-serwis.png" class="img-fluid counter-number--icon" alt="">
-                    <p class="count counter-number counter-number--plus standard-title-3 chakra-petch-font fw-bold" data-count="8000">0</p>
+                    <p class="count counter-number counter-number--plus standard-title-3 chakra-petch-font fw-bold" data-count="20000">0</p>
                 </div>
-                <p class="text-uppercase fw-bold counter-main-box--titles lh-13">Naprawionych układów klimatyzacji</p>
+                <p class="text-uppercase fw-bold counter-main-box--titles lh-13">Sprzedanych egzemplarzy</p>
             </div>
             <div class="col-lg-3">
                 <div class="position-relative counter-number--box">
