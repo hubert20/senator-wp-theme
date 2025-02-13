88b1618713ef730d65596f5a29d92c829dea4ecf
Hubert <hubert@tomedia.pl>
2025-02-13 15:47:01 +0100

    update

--- footer-widget.php
+++ footer-widget.php
@@ -9,60 +9,11 @@
         <div class="footer-widgets__logo mb-4 mb-lg-5"></div>
         <div class="row justify-content-center">
             <div class="col-lg-4 footer-widgets__bottom-desc pe-lg-5 mb-4 mb-lg-0">
-                <h5 class="text-yellow standard-title-7 playfair-petch-font mb-2 mb-lg-4 fw-bold">O nas</h5>
-                <p class="bottom fw-lighter">
-                    Od 1997 dostarczamy szafy na wymiar wraz z systemem drzwi przesuwnych. 
-                    W naszej ofercie znajdują się również pozostałe elementy akcesoriów potrzebnych do zaprojektowania każdego pomieszczenia.
-                    Działając od wielu lat śmiało możemy stwierdzić, że jesteśmy jednym z liderów na polskim rynku.
-                    Senator sp. z o. o. specjalizuje się w sprzedaży hurtowej i detalicznej systemów drzwi przesuwnych i szaf garderobianych.
-                    Ponadto sprzedajemy akcesoria meblowe od drewnianych kołków dla stolarzy po rączki, uchwyty i prowadnice dla klienta indywidualnego.
-                    Nasz asortyment zawiera produkty niezbędne do montażu mebli oraz wyposażenia wnętrz w takich miejscach jak: kuchnia, salon, łazienka, garderoba oraz w wielu innych pomieszczeniach.
-                </p>
-                <p class="bottom fw-lighter">
-                    Współpraca z nami to gwarancja jakości połączonej z pasją do wygody i luksusu.
-                </p>
-                <a href="https://www.facebook.com/hatek.hatek.902" target="_blank" rel="nofollow noopener noreferrer" class="text-center d-inline-block text-yellow bottom-fb-link">
-                    <i class="fa fa-facebook text-green"></i>
-                </a>
-            </div>
-            <div class="col-lg-3 footer-widgets__bottom-desc mb-4 mb-lg-0">
-                <h5 class="text-yellow standard-title-7 playfair-petch-font mb-2 mb-lg-4 fw-bold">Kontakt</h5>
-                <ul class="list-unstyled p-0 bottom-contact">
-                    <li class="mb-3 mb-lg-4 d-flex align-items-center">
-                        <i class="fa fa-map-marker me-4 text-yellow"></i>
-                        <span>
-                            Senator sp. z o. o. <br>
-                            09-500 Gostynin<br>
-                            ul. Płocka 37,<br>
-                            woj. mazowieckie
-                        </span>
-                    </li>
-                    <li class="mb-2 d-flex align-items-center">
-                        <i class="fa fa-phone me-4 text-yellow"></i>
-                        <a href="tel:+48242353645" class="">
-                            +48 24 235 36 45
-                        </a>
-                    </li>
-                    <li class="mb-3 mb-lg-4 d-flex align-items-center">
-                        <i class="fa fa-fax me-4 text-yellow"></i>
-                        <a href="tel:+48242353645" class="">
-                            +48 24 235 33 81
-                        </a>
-                    </li>
-                    <li class="d-flex align-items-center">
-                        <i class="fa fa-envelope me-4 text-yellow"></i>
-                        <a href="mailto: biuro@senator.com.pl" class="">
-                            biuro@senator.com.pl
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-lg-4 footer-widgets__bottom-desc mb-4 mb-lg-0">
                 <?php if (is_active_sidebar('menu-about')) : ?>
                     <?php dynamic_sidebar('menu-about'); ?>
                 <?php endif; ?>
             </div>
-            <div class="col-lg-4 footer-widgets__bottom-desc mb-4 mb-lg-0">
+            <div class="col-lg-3 footer-widgets__bottom-desc mb-4 mb-lg-0">
                 <?php if (is_active_sidebar('footer-start')) : ?>
                     <?php dynamic_sidebar('footer-start'); ?>
                 <?php endif; ?>
